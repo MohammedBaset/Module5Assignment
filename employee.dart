dac899dcd348de525952854498778c1e5fbf6e27
Mohammed Baset <basetemail@gmail.com>
2025-09-14 07:26:50 +1000

    Add Employee, Manager, and Developer classes for Module 5 Assignment

--- employee.dart
+++ employee.dart
@@ -4,25 +4,25 @@ void main(){
   print(" ");
 
   // Create a Manager object with name, department, and salary
-  Manager John = Manager('John', 'Manager', 90000);
-  John.infodisplay();
+  Manager john = Manager('John', 'Manager', 90000);
+  john.infodisplay();
   print(" ");
 
   // Create a Developer object with name, programming language, and salary
-  Developer Alex = Developer('Alex', 'Dart', 80000);
-  Alex.infodisplay();
+  Developer alex = Developer('Alex', 'Dart', 80000);
+  alex.infodisplay();
 }
 // --------------------- Employee Base Class ---------------------
 class Employee{
   String name; // Employee's name
   double salary; // Employee's salary
-  Employee(this.name, this.salary){} // Constructor for Employee class
+  Employee(this.name, this.salary); // Constructor for Employee class
 }
 // --------------------- Manager Class ---------------------
 class Manager extends Employee{
   String department; // Manager-specific property
 
-  Manager(super.name, this.department, super.salary){}  // Constructor: calls Employee constructor (name, salary) and initializes department
+  Manager(super.name, this.department, super.salary);  // Constructor: calls Employee constructor (name, salary) and initializes department
 
 // Method to display Manager's details
   void infodisplay(){
@@ -33,7 +33,7 @@ class Manager extends Employee{
 // --------------------- Developer Class ---------------------
 class Developer extends Employee{
   String programmingLanguage; // Developer-specific property
-  Developer(super.name, this.programmingLanguage, super.salary){} // Constructor: calls Employee constructor (name, salary) and initializes programming Language
+  Developer(super.name, this.programmingLanguage, super.salary); // Constructor: calls Employee constructor (name, salary) and initializes programming Language
 
   // Method to display Developer's details
   void infodisplay(){
